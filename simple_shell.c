218b10fc1554cbe5429ba9a4355bcdd31a45a91b
p-omeki <partymeki@gmail.com>
2023-08-15 23:31:46 -0700

    16

---
+++ simple_shell.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+extern char **environ;
+
+/**
+ * get_variable_value - Gets the value of a specified variable.
+ * @variable: The name of the variable.
+ * Return: The value of the variable.
+ */
+const char *get_variable_value(const char *variable) {
+    /* ... (same implementation as before) */
+    (void)variable; /* To suppress unused parameter warning */
+}
+
+/**
+ * execute_command - Executes a command and handles variable replacement.
+ * @command: The command to execute.
+ */
+void execute_command(const char *command) {
+    /* ... (same implementation as before) */
+    (void)command; /* To suppress unused parameter warning */
+}
+
+/**
+ * main - Simple shell implementation with file execution support.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[]) {
+    char input[BUFFER_SIZE];
+    ssize_t read_size;
+
+    if (argc > 1) {
+        /* Reading commands from a file */
+        FILE *file = fopen(argv[1], "r");
+        if (file == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+
+        while (fgets(input, BUFFER_SIZE, file) != NULL) {
+            input[strcspn(input, "\n")] = '\0'; /* Remove newline character */
+            execute_command(input);
+        }
+
+        fclose(file);
+    } else {
+        /* Interactive shell mode */
+        while (1) {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "($) ", 5);
+
+            read_size = read(STDIN_FILENO, input, BUFFER_SIZE);
+            if (read_size <= 0) {
+                if (isatty(STDIN_FILENO))
+                    write(STDOUT_FILENO, "\n", 1);
+                break;
+            }
+            input[read_size - 1] = '\0';
+
+            execute_command(input);
+        }
+    }
+
+    return 0;
+}
+
